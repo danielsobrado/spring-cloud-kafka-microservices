1e38efc43937cb860f5cdafddf987bf463f8867d
Daniel Sobrado <daniel@danielsobrado.com>
2022-08-16 22:13:31 +0400

    Circuit Breaker on API Gateway

--- jvmcc-api-gateway/src/main/java/com/jds/jvmcc/jvmccapigateway/JvmccApiGatewayApplication.java
+++ jvmcc-api-gateway/src/main/java/com/jds/jvmcc/jvmccapigateway/JvmccApiGatewayApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author J. Daniel Sobrado
