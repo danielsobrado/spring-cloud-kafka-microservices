d985d8c6a6ded074ed8922d280e7039c3d9054c7
Daniel Sobrado <daniel@danielsobrado.com>
2022-08-20 10:13:51 +0400

    LoggingFilter for API Gateway

---
+++ jvmcc-api-gateway/src/main/java/com/jds/jvmcc/jvmccapigateway/filter/LoggingFilter.java
@@ -0,0 +1,39 @@
+package com.jds.jvmcc.jvmccapigateway.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
+
+/**
+ * @author J. Daniel Sobrado
+ * @version 1.0
+ * @since 2022-08-20
+ */
+@Component
+public class LoggingFilter implements GlobalFilter {
+    Log log = LogFactory.getLog(getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+        String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        log.info("Incoming request " + originalUri + " is routed to id: " + route.getId()
+                + ", uri:" + routeUri);
+        return chain.filter(exchange);
+    }
+}
