0e217f18759df4b3bcc64de4066b08281bd24517
Daniel Sobrado <daniel@danielsobrado.com>
2022-08-29 19:37:35 +0400

    update feign

--- jvmcc-product-service/src/main/java/com/jds/jvmcc/productservice/client/FeignReviewClientProduction.java
+++ jvmcc-product-service/src/main/java/com/jds/jvmcc/productservice/client/FeignReviewClientProduction.java
@@ -14,7 +14,6 @@ import com.jds.jvmcc.productservice.fallback.ReviewFallback;
  */
 @Profile("production")
 @FeignClient(value = "jvmcc-review", 
-    url = "http://jvmcc-review-service",
     configuration = LocalClientConfiguration.class, 
     fallback = ReviewFallback.class)
 @RibbonClient(name = "jvmcc-review")
