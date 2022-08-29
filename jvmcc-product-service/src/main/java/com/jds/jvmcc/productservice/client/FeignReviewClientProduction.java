3631d6bd03cfc3ccba707aad3a94b0ed019f2267
Daniel Sobrado <daniel@danielsobrado.com>
2022-08-29 10:50:13 +0400

    under ssl

--- jvmcc-product-service/src/main/java/com/jds/jvmcc/productservice/client/FeignReviewClientProduction.java
+++ jvmcc-product-service/src/main/java/com/jds/jvmcc/productservice/client/FeignReviewClientProduction.java
@@ -14,7 +14,7 @@ import com.jds.jvmcc.productservice.fallback.ReviewFallback;
  */
 @Profile("production")
 @FeignClient(value = "jvmcc-review-service-1", 
-    url = "http://jvmcc-review-service-1:8080, http://jvmcc-review-service-2:8081",
+    url = "https://jvmcc-review-service-1:8080, https://jvmcc-review-service-2:8081",
     configuration = LocalClientConfiguration.class, 
     fallback = ReviewFallback.class)
 @RibbonClient(name = "jvmcc-review-service-1")
