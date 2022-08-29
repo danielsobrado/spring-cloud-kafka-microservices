175aa262376687808f8ed975fef5d16e3ddd203b
Daniel Sobrado <daniel@danielsobrado.com>
2022-08-29 10:49:57 +0400

    Feign using api gateway url

--- jvmcc-product-service/src/main/java/com/jds/jvmcc/productservice/client/FeignReviewClientProduction.java
+++ jvmcc-product-service/src/main/java/com/jds/jvmcc/productservice/client/FeignReviewClientProduction.java
@@ -13,10 +13,10 @@ import com.jds.jvmcc.productservice.fallback.ReviewFallback;
  * @since 2022-08-29
  */
 @Profile("production")
-@FeignClient(value = "jvmcc-review-service", 
-    url = "http://jvmcc-review-service-1:8093, http://jvmcc-review-service-2:8094",
+@FeignClient(value = "jvmcc-review-service-1", 
+    url = "http://jvmcc-review-service-1:8080, http://jvmcc-review-service-2:8081",
     configuration = LocalClientConfiguration.class, 
     fallback = ReviewFallback.class)
-@RibbonClient(name = "jvmcc-review-service")
+@RibbonClient(name = "jvmcc-review-service-1")
 public interface FeignReviewClientProduction extends ReviewClient {
 }
